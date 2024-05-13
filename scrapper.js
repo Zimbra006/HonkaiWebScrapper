7b519cc9d6f00ee574aff4c2be5f3217d25ce6f8
Guilherme Amazonas Böhme <guibohme@gmail.com>
2024-05-13 20:24:37 -0300

    Revert "Now it doesn't crash if the page doesn't have any"
    This reverts commit bf2948f5717d407abd90c685b6eef1c960880d9c.

--- scrapper.js
+++ scrapper.js
@@ -1,22 +1,17 @@
 const puppeteer = require('puppeteer');
 const charactersUrls = require('./charactersUrls.json');
 
-const main = async (name) => {
+const main = async () => {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
 
-    // for(let index = 0; index < 3; index++){
-    //     let element = charactersUrls[index];
-    //     let name = element.name;
-    //     let build = await scrapeStats(page, element.url);
-
-    //     console.log({name, build});
-    // }
-
-    const character = charactersUrls.find((element) => element.name == name);
-
-    let build = await scrapeStats(page, character.url);
-    console.log({ build });
+    for(let index = 0; index < 3; index++){
+        let element = charactersUrls[index];
+        let name = element.name;
+        let build = await scrapeStats(page, element.url);
+        
+        console.log({name, build});
+    }
 
     await browser.close();
 };
@@ -26,26 +21,15 @@ async function scrapeStats(page, url) {
     await page.goto(url);
 
     const build = await page.evaluate(() => {
-        try {
-            const buildStats = document.querySelector('div.build-stats');
-            const stats = buildStats.querySelector('div.list').querySelectorAll('li');
-            return Array.from(stats).map((stats) => {
-                const stat = stats.querySelector('p').innerText;
-                return stat;
-            });
-        } catch (err) { return "Sorry, could not find any info."};
+        const buildStats = document.querySelector('div.build-stats');
+        const stats = buildStats.querySelector('div.list').querySelectorAll('li');
+        return Array.from(stats).map((stats) => {
+            const stat = stats.querySelector('p').innerText;
+            return stat;
+        });
     });
 
     return build;
 };
 
-const readline = require('readline').createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
-
-readline.question('Enter a character name: ', name => {
-    console.log(`Looking for information on ${name}`);
-    main(name);
-    readline.close();
-});
+main();
