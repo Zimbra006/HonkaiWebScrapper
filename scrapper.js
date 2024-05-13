bf2948f5717d407abd90c685b6eef1c960880d9c
Guilherme Amazonas Böhme <guibohme@gmail.com>
2024-05-13 20:18:19 -0300

    Now it doesn't crash if the page doesn't have any info.

--- scrapper.js
+++ scrapper.js
@@ -1,17 +1,22 @@
 const puppeteer = require('puppeteer');
 const charactersUrls = require('./charactersUrls.json');
 
-const main = async () => {
+const main = async (name) => {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
 
-    for(let index = 0; index < 3; index++){
-        let element = charactersUrls[index];
-        let name = element.name;
-        let build = await scrapeStats(page, element.url);
-        
-        console.log({name, build});
-    }
+    // for(let index = 0; index < 3; index++){
+    //     let element = charactersUrls[index];
+    //     let name = element.name;
+    //     let build = await scrapeStats(page, element.url);
+
+    //     console.log({name, build});
+    // }
+
+    const character = charactersUrls.find((element) => element.name == name);
+
+    let build = await scrapeStats(page, character.url);
+    console.log({ build });
 
     await browser.close();
 };
@@ -21,15 +26,26 @@ async function scrapeStats(page, url) {
     await page.goto(url);
 
     const build = await page.evaluate(() => {
-        const buildStats = document.querySelector('div.build-stats');
-        const stats = buildStats.querySelector('div.list').querySelectorAll('li');
-        return Array.from(stats).map((stats) => {
-            const stat = stats.querySelector('p').innerText;
-            return stat;
-        });
+        try {
+            const buildStats = document.querySelector('div.build-stats');
+            const stats = buildStats.querySelector('div.list').querySelectorAll('li');
+            return Array.from(stats).map((stats) => {
+                const stat = stats.querySelector('p').innerText;
+                return stat;
+            });
+        } catch (err) { return "Sorry, could not find any info."};
     });
 
     return build;
 };
 
-main();
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+readline.question('Enter a character name: ', name => {
+    console.log(`Looking for information on ${name}`);
+    main(name);
+    readline.close();
+});
